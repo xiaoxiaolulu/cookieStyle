8417f716a63d4b6e9dd64771639ae69791c0c06c
Null <546464268@qq.com>
2020-03-31 13:25:03 +0800

    问答模块完成

---
+++ test/chat/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class ChatConfig(AppConfig):
+
+    name = 'test.chat'
+    verbose_name = "聊天管理"
---
+++ test/chat/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from test.chat import views
+
+
+app_name = "chat"
+
+urlpatterns = [
+]
